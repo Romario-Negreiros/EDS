5f2a22e03e27bd328e849b0957ed36e1913fb1f7
Romario Negreiros Baldarenas Silva <nromario482@gmail.com>
2023-10-18 20:55:33 -0300

    Create AppCard component and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,54 @@
+import AppCard from "@/components/Card";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
 export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  )
+    return (
+        <Box sx={{ padding: 2 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <AppCard
+                        author="Romario"
+                        title="Lorem ipsum dolor sit amet"
+                        previewContent="
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur recusandae debitis culpa dicta vitae nulla consectetur inventore sit corrupti quaerat reiciendis suscipit, laborum natus dolores veniam officia animi obcaecati placeat excepturi soluta, fuga distinctio laboriosam ducimus! Amet ipsam illo possimus placeat soluta quo voluptatibus reprehenderit repellat enim itaque accusamus officia, autem debitis quidem quas, exercitationem rerum ullam. Neque, nesciunt ab?
+"
+                        id={1}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    <AppCard
+                        author="Romario"
+                        title="Lorem ipsum dolor sit amet"
+                        previewContent="
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur recusandae debitis culpa dicta vitae nulla consectetur inventore sit corrupti quaerat reiciendis suscipit, laborum natus dolores veniam officia animi obcaecati placeat excepturi soluta, fuga distinctio laboriosam ducimus! Amet ipsam illo possimus placeat soluta quo voluptatibus reprehenderit repellat enim itaque accusamus officia, autem debitis quidem quas, exercitationem rerum ullam. Neque, nesciunt ab?
+"
+                        id={2}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    {" "}
+                    <AppCard
+                        author="Romario"
+                        title="Lorem ipsum dolor sit amet"
+                        previewContent="
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur recusandae debitis culpa dicta vitae nulla consectetur inventore sit corrupti quaerat reiciendis suscipit, laborum natus dolores veniam officia animi obcaecati placeat excepturi soluta, fuga distinctio laboriosam ducimus! Amet ipsam illo possimus placeat soluta quo voluptatibus reprehenderit repellat enim itaque accusamus officia, autem debitis quidem quas, exercitationem rerum ullam. Neque, nesciunt ab?
+"
+                        id={3}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                    {" "}
+                    <AppCard
+                        author="Romario"
+                        title="Lorem ipsum dolor sit amet"
+                        previewContent="
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur recusandae debitis culpa dicta vitae nulla consectetur inventore sit corrupti quaerat reiciendis suscipit, laborum natus dolores veniam officia animi obcaecati placeat excepturi soluta, fuga distinctio laboriosam ducimus! Amet ipsam illo possimus placeat soluta quo voluptatibus reprehenderit repellat enim itaque accusamus officia, autem debitis quidem quas, exercitationem rerum ullam. Neque, nesciunt ab?
+"
+                        id={4}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+
+interface Props {
+    id: number;
+    author: string;
+    title: string;
+    previewContent: string;
+    disableSecondLink?: boolean;
+}
+
+export default function AppCard({
+    id,
+    author,
+    title,
+    previewContent,
+    disableSecondLink,
+}: Props) {
+    return (
+        <Card>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {previewContent}...
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">
+                    <Link href={`/${author.toLowerCase()}/${id}`}>
+                        Ler tudo
+                    </Link>
+                </Button>
+                {!disableSecondLink && (
+                    <Button size="small">
+                        <Link href={`/${author.toLowerCase()}`}>Mais de</Link>
+                    </Button>
+                )}
+            </CardActions>
+        </Card>
+    );
+}
